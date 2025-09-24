baf38e46978527ccb74e9a20d12110c0f843e56f
Arockia Varsha M <cchithramary@gmail.com>
2025-09-24 05:37:36 +0530

    Create db.js

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("loginauth", "root", "password", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+sequelize.authenticate()
+  .then(() => console.log("✅ MySQL Connected"))
+  .catch((err) => {
+    console.error("❌ Database connection failed:", err.message);
+    process.exit(1);
+  });
+
+module.exports = sequelize;
+
